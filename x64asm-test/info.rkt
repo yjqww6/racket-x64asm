23f0e4e2d6e93b8c16f3826ade98beca8c546880
yjqww6 <343519265@qq.com>
2019-09-22 18:23:19 +0800

    update deps

--- x64asm-test/info.rkt
+++ x64asm-test/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection 'multi)
-(define deps '("x64asm-lib" "base" "typed-racket-lib"))
-(define build-deps '("rackunit-typed" "rackunit-lib" "math-lib"))
+(define deps '("x64asm-lib" "base" "typed-racket-lib"
+                            "rackunit-typed" "rackunit-lib" "math-lib"))
 (define pkg-desc "a simple x64 assembler written in typed/racket")
 (define version "0.1")
 (define pkg-authors '(yjqww6))
