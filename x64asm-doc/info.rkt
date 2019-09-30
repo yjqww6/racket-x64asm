6a6acaf861102d0a9da2033ae4ac85c4be1a1690
yjqww6 <343519265@qq.com>
2019-09-30 22:49:36 +0800

    bump

--- x64asm-doc/info.rkt
+++ x64asm-doc/info.rkt
@@ -4,6 +4,7 @@
 (define build-deps '("x64asm-lib" "scribble-lib" "racket-doc"
                                   "typed-racket-doc"
                                   "typed-racket-lib"))
+(define update-implies '("x64asm-lib"))
 (define pkg-desc "a simple x64 assembler written in typed/racket")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(yjqww6))
--- x64asm/info.rkt
+++ x64asm/info.rkt
@@ -3,5 +3,5 @@
 (define deps '("x64asm-lib" "x64asm-doc"))
 (define implies '("x64asm-lib" "x64asm-doc"))
 (define pkg-desc "a simple x64 assembler written in typed/racket")
-(define version "0.1")
+(define version "0.2")
 (define pkg-authors '(yjqww6))
