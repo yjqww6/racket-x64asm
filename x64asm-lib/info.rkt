b8a4e9998428f4f0b1d083d74d9730e8369f0110
yjqww6 <343519265@qq.com>
2021-04-06 13:16:20 +0800

    bump version

--- x64asm-lib/info.rkt
+++ x64asm-lib/info.rkt
@@ -2,5 +2,5 @@
 (define collection 'multi)
 (define deps '("base" "typed-racket-lib"))
 (define pkg-desc "a simple x64 assembler written in typed/racket")
-(define version "0.2")
+(define version "0.3")
 (define pkg-authors '(yjqww6))
